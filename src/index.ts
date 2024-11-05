fd64998ed86557efd36ba174ecae9ce366386ddf
Josh Conley <joshconley@Joshs-MBP.attlocal.net>
2024-11-05 11:27:28 -0500

    updating week 9

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,15 @@ async function main() {
   const league = new LeagueService(sleeper);
   const playoffs = new PlayoffService(sleeper);
   const stats = new StatisticsService();
-  // const data = await league.getMatchupsForAllLeagues();
-  const data = stats.buildRegularSeasonAllTimeStats();
+  const allMatchups = await league.getMatchupsForAllLeagues();
+  // const data = stats.buildRegularSeasonAllTimeStats();
   // const data = await sleeper.getWinnersBracket(LEAGUE_IDS["PREMIER"][2023]);
   // const data = await sleeper.getLosersBracket(LEAGUE_IDS["PREMIER"][2023]);
   // const data = await playoffs.getPlayoffsForLeague(LEAGUE_IDS["PREMIER"][2023]);
   // const data = await league.getMatchupsForWeek(LEAGUE_IDS.NATIONAL[2024], 7);
   // const user = await sleeper.getRosters(LEAGUE_IDS.NATIONAL[2024]);
-  // fs.writeFileSync('./output/all-matchups.json', JSON.stringify(data));   
-  fs.writeFileSync('./output/stats.json', data);     
+  fs.writeFileSync('./output/all-matchups.json', JSON.stringify(allMatchups));   
+  // fs.writeFileSync('./output/stats.json', data);     
 }
 
 main();
