59ca88e65e0084c01ca9c161363a5918382fc2e4
Josh Conley <joshconley@Joshs-MBP.attlocal.net>
2024-11-26 15:31:08 -0500

    barebones static site (from ffu sleeper service)

--- src/services/player.service.ts
+++
@@ -1,9 +0,0 @@
-import players from '../config/player-data.json' with { type: "json" };;
-
-export class PlayerService {
-    
-    public getPlayerById(term: string) {
-        return players['1' as keyof Object];
-    }
-
-}
